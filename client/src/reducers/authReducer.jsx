c7dae10e2193609e3f76028509dd7a0fb852adae
skyler dowdy <skylerwebdev@gmail.com>
2019-08-23 11:38:16 -0400

    authreducer

---
+++ client/src/reducers/authReducer.jsx
@@ -0,0 +1,33 @@
+export default (state = initialState, action) => {
+    const { type, payload } = action;
+    switch (type) {
+  
+      case types.LOGIN_START:
+        return {
+          ...state,
+          isLoading: true,
+          errors: null,
+          isAuth: false,
+          isSuccess: false
+        };
+      case types.LOGIN_SUCCESS:
+        return {
+          ...state,
+          isLoading: false,
+          isAuth: true,
+          token: payload.token,
+          user: payload.user,
+          isSuccess: true
+        };
+      case types.LOGIN_FAIL:
+        return {
+          ...state,
+          errors: payload,
+          isLoading: false,
+        };
+
+      default:
+        return state;
+    }
+  };
+  
