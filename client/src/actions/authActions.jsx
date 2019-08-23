2779a32ba4ab09a18f260b0f4cb9514ef04d02c9
skyler dowdy <skylerwebdev@gmail.com>
2019-08-23 18:06:52 -0400

    got colors displaying

--- client/src/actions/authActions.jsx
+++ client/src/actions/authActions.jsx
@@ -2,26 +2,20 @@ import axiosWithAuth from '../utils/axiosWithAuth'
 import { types }from './index'
 
 
-
-export const doSignIn = credentials => dispatch => {
-
-  dispatch({ type: types.LOGIN_START});
   return axiosWithAuth()
     .post('/login', credentials)
     .then(
       res => {
         console.log(res)
         localStorage.setItem('token', (res.data.payload));
-        dispatch({type: types.LOGIN_SUCCESS, payload: res.data.payload});
       }
     )
     .catch(
       err => {
-        dispatch({type: types.LOGIN_FAIL, payload: err})
-        console.log(err)
+        console.log(err.response)
       }
     )
-};
+
 
 
 
