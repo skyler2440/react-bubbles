4fd7e6e8cb2bc7dccb45ea95b39eb34fb04d51c3
skyler dowdy <skylerwebdev@gmail.com>
2019-08-23 11:41:58 -0400

    item reducer

---
+++ client/src/reducers/itemReducer.jsx
@@ -0,0 +1,106 @@
+import { types } from "../actions";
+
+const initialState = {
+  rentItems: [],
+  isLoading: false,
+  errors: null,
+  isSuccess: true
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+
+    case types.GET_ITEM_START:
+      return {
+        ...state,
+        isLoading: true,
+        errors: null
+      };
+    case types.GET_ITEM_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        errors: null,
+        rentItems: action.payload
+      };
+    case types.GET_ITEM_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        errors: payload.err
+      };
+
+    case types.POST_ITEM_START:
+      return {
+        ...state,
+        isLoading: true,
+        errors: null
+      };
+    case types.POST_ITEM_SUCCESS:
+      const updatedPOSTEquipmentITEM = [...state.equipment, payload];
+      return {
+        ...state,
+        isLoading: false,
+        errors: null,
+        rentItems: updatedPOSTEquipmentITEM,
+        isSuccess: true
+      };
+    case types.POST_ITEM_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        errors: payload,
+        isSuccess: false
+      };
+
+    case types.PUT_ITEM_START:
+      return {
+        ...state,
+        isLoading: true,
+        errors: null
+      };
+    case types.PUT_ITEM_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        errors: null,
+        equipment: payload,
+        isSuccess: true
+      };
+    case types.PUT_ITEM_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        errors: payload,
+        isSuccess: false
+      };
+
+    case types.DELETE_START:
+      return {
+        ...state,
+        isLoading: true,
+        errors: null
+      };
+
+    case types.DELETE_SUCCESS:
+      const updatedEquipment = state.equipment.filter(
+        item => item.id !== payload
+      );
+      return {
+        ...state,
+        isLoading: false,
+        errors: null,
+        equipment: updatedEquipment
+      };
+
+    case types.DELETE_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        errors: payload
+      };
+    default:
+      return state;
+  }
+};
